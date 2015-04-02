a4d377d3ab119fa323128165d3c4cbfd141f2a6b
Ohad Rodeh <orodeh@dnanexus.com>
2015-04-02 00:47:45 -0700

    Trying fix suggested by Paradigm4

--- PyVCF_titv/titv.py
+++ PyVCF_titv/titv.py
@@ -2,11 +2,23 @@ import vcf
 
 filename = "data/ALL.chr21.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz"
 
-
-def read_and_process(rec):
-    print rec.REF, rec.ALT
+# We have a single-letter biallelic SNP.
+# Check if this is a transversion or inversion
+def count_snp(ref, alt):
+    pass
 
 vcf_reader = vcf.Reader(open(filename, 'r'))
-for i in range(0,9):
+for i in range(0,10):
     rec = vcf_reader.next()
-    read_and_process(rec)
+    ref = rec.REF
+    alt = rec.ALT
+    if (ref is None or
+        alt is None):
+        continue
+    if len(alt) != 1:
+        continue
+    alt = alt[0]
+    if (len(ref) != 1 or len(alt) != 1):
+        continue
+    # we now have a biallelic SNP
+    count_snp(ref, alt)
--- problem_desc_standalone.R
+++ problem_desc_standalone.R
@@ -10,4 +10,19 @@ A <- aggregate(m, by=list("vid", "x", "y"), FUN="count(*)", eval=TRUE)
 # This aggregation succeeds
 B <- aggregate(m, by=list("vid"), FUN="count(*)", eval=TRUE)
 
+## ---- Rewrite with int64 types ----------
+# Build an array with three attributes per cell
+m = scidb("build(<vid:int64>[i=0:100000,1000,0], random()%10000)")
+m = bind(m, "x", "int64(random()%2)")
+m = bind(m, "y", "int64(random()%2)")
+m = scidbeval(m)
+
+m1 <- bind(m,"one",1)
+
+# fails with a syntax error
+A <- redimension(m1,
+                 dim=c("vid","x","y"),
+                 FUN="sum(one) as sum")
+
+
 
