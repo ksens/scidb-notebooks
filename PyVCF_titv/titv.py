ccaabb3495efb4cda01fe26de2418ea0dbea3c5c
Ohad Rodeh <orodeh@dnanexus.com>
2015-04-02 03:23:59 -0700

    Initial version of Python script

--- PyVCF_titv/titv.py
+++ PyVCF_titv/titv.py
@@ -1,24 +1,60 @@
 import vcf
+import pprint
 
 filename = "data/ALL.chr21.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz"
 
-# We have a single-letter biallelic SNP.
-# Check if this is a transversion or inversion
-def count_snp(ref, alt):
-    pass
+ti = 0
+tv = 0
 
-vcf_reader = vcf.Reader(open(filename, 'r'))
-for i in range(0,10):
-    rec = vcf_reader.next()
+def is_transition(ref, alt):
+    if ((ref == 'A' and alt == 'G') or
+        (ref == 'G' and alt == 'A') or
+        (ref == 'C' and alt == 'T') or
+        (ref == 'T' and alt == 'C')):
+        return True
+    else:
+        return False
+
+def account_rec(rec):
+    global ti, tv
     ref = rec.REF
     alt = rec.ALT
     if (ref is None or
         alt is None):
-        continue
+        return
+    if not type(alt) is list:
+        return
     if len(alt) != 1:
-        continue
+        return
     alt = alt[0]
+    if not type(alt) is vcf.model._Substitution:
+        return
     if (len(ref) != 1 or len(alt) != 1):
-        continue
+        return
+
     # we now have a biallelic SNP
-    count_snp(ref, alt)
+    if is_transition(ref, alt):
+        ti += 1
+    else:
+        tv += 1
+
+# This version uses PyVCF functions.
+# There are slight differences, nothing major.
+def account_rec_native(rec):
+    global ti, tv
+    if not rec.is_snp:
+        return
+    if rec.is_transition:
+        ti += 1
+    else:
+        tv += 1
+
+with open(filename, 'r') as f:
+    vcf_reader = vcf.Reader(f)
+    #for i in range(0,10000):
+    while True:
+        rec = vcf_reader.next()
+        #account_rec(rec)
+        account_rec_native(rec)
+
+print("ti={}  tv={}".format(ti, tv))
